70d4236e08e6d81ab131f84f927e5b9fcd409a82
Tiffany B <tjbrincefield@gmail.com>
2019-03-01 20:05:05 -0500

    added images and some html code

---
+++ assets/javascript/game.js
@@ -0,0 +1,114 @@
+
+//sets up random number user is trying to match
+$( document ).ready(function(){
+    var Random=Math.floor(Math.random()*101+19)
+    console.log(Random)
+    $('#number').text(Random);
+
+//	setting up four random numbers array, total variable and printing the wins & losses variables	
+var GeneratedNumbers = []
+var total= 0;	
+var wins= 0;
+var losses = 0;
+$('#numberWins').text(wins);
+$('#numberLosses').text(losses);		
+
+
+//sets up random numbers for each jewel
+function fourRandomNumbers(){
+for (var i = 0; i < 4; i++) {
+    var num = Math.floor(Math.random()*11+1);
+    GeneratedNumbers.push(num);
+}
+console.log(GeneratedNumbers) 
+}
+
+fourRandomNumbers();
+
+
+//resets the game
+function reset(){
+    Random=Math.floor(Math.random()*101+19);
+    console.log(Random)
+    $('#number').text(Random);
+    GeneratedNumbers = [];
+    fourRandomNumbers();
+    total= 0;
+    $('#Combined').text(total);
+    } 
+
+//adds the wins to the total
+function yay(){
+alert("You won!!!");
+    wins++;	
+    $('#numberWins').text(wins);
+    reset();
+}
+
+//adds the losses to the total
+function loser(){
+alert ("You lose!!");
+    losses++;
+    $('#numberLosses').text(losses);
+    reset();
+}
+
+
+
+//sets up click for jewels
+    $('#one').on ('click', function(){
+        total = total + GeneratedNumbers[0];
+        console.log("New total= " + total);
+        $('#Combined').text(total);	
+
+                    //sets win/lose conditions
+                if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }		
+    })	
+
+    $('#two').on ('click', function(){
+        total = total + GeneratedNumbers[1];
+        console.log("New total= " + total);
+        $('#Combined').text(total);	
+
+                if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }	
+    })	
+
+    $('#three').on ('click', function(){
+        total = total + GeneratedNumbers[2];
+        console.log("New total= " + total);
+        $('#Combined').text(total);
+
+//sets win/lose conditions
+                    if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }	
+    })	
+
+    $('#four').on ('click', function(){
+        total = total + GeneratedNumbers[3];
+        console.log("New total= " + total);
+        $('#Combined').text(total);	
+
+            
+                    if (total == Random){
+                    yay();
+                }
+                else if ( total > Random){
+                    loser();
+                }
+    });  	
+
+});
